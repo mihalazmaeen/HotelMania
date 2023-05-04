55495ee7a916d38654b46fcb397ee86f1ad0b07f
mihalazmaeen <azmaeenmihal1@gmail.com>
2023-05-04 12:10:41 +0600

    admin template mastering

---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,58 @@
+<div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Admin Panel</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html"></a>
+        </div>
+
+        <ul class="sidebar-menu">
+
+            <li class="active"><a class="nav-link" href="index.html"><i class="fas fa-hand-point-right"></i> <span>Dashboard</span></a></li>
+
+            <li class="nav-item dropdown active">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-hand-point-right"></i><span>Dropdown Items</span></a>
+                <ul class="dropdown-menu">
+                    <li class="active"><a class="nav-link" href=""><i class="fas fa-angle-right"></i> Item 1</a></li>
+                    <li class=""><a class="nav-link" href=""><i class="fas fa-angle-right"></i> Item 2</a></li>
+                </ul>
+            </li>
+
+            <li class=""><a class="nav-link" href="setting.html"><i class="fas fa-hand-point-right"></i> <span>Setting</span></a></li>
+
+            <li class=""><a class="nav-link" href="form.html"><i class="fas fa-hand-point-right"></i> <span>Form</span></a></li>
+
+            <li class=""><a class="nav-link" href="table.html"><i class="fas fa-hand-point-right"></i> <span>Table</span></a></li>
+
+            <li class=""><a class="nav-link" href="invoice.html"><i class="fas fa-hand-point-right"></i> <span>Invoice</span></a></li>
+
+        </ul>
+    </aside>
+</div>
+<div class="navbar-bg"></div>
+<nav class="navbar navbar-expand-lg main-navbar">
+    <form class="form-inline mr-auto">
+        <ul class="navbar-nav mr-3">
+            <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+            <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
+        </ul>
+    </form>
+    <ul class="navbar-nav navbar-right">
+        <li class="nav-link">
+            <a href="" target="_blank" class="btn btn-warning">Front End</a>
+        </li>
+        <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                <img alt="image" src="{{asset('uploads/user.jpg')}}" class="rounded-circle mr-1">
+                <div class="d-sm-none d-lg-inline-block">John Doe</div></a>
+            <div class="dropdown-menu dropdown-menu-right">
+                <a href="profile.html" class="dropdown-item has-icon">
+                    <i class="far fa-user"></i> Edit Profile
+                </a>
+                <a href="login.html" class="dropdown-item has-icon text-danger">
+                    <i class="fas fa-sign-out-alt"></i> Logout
+                </a>
+            </div>
+        </li>
+    </ul>
+</nav>
